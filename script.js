34384dd19ba38db54910fd114eca8c6ee612fbac
Kumar9525 <92426822+Kumar9525@users.noreply.github.com>
2024-01-30 21:45:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,6 +10,6 @@ return (typeof valu1 === typeof value2) || (isNaN(value1) && isNaN(value2));
 }
 
 // do not change the code below.
-// let value1 = prompt("Enter Start of the Range.");
-//  let value2 = prompt("Enter End Of the Range.");
+let value1 = prompt("Enter Start of the Range.");
+ let value2 = prompt("Enter End Of the Range.");
 alert(isSameType(value1, value2));
